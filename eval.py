bcb78c9c5c6e41af90be6e5bfb3044dc3fc738fe
Eli Chen <ita3051@gmail.com>
2022-06-21 19:34:53 +0800

    Build cv-related scroing functions

--- eval.py
+++ eval.py
@@ -1,3 +1,55 @@
+import logging
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_auc_score
+
+
+scoring_maps = {
+    accuracy_score: "accuracy",
+    recall_score: "recall",
+    precision_score: "precision",
+    roc_auc_score: "roc_auc",
+}
+
+scoring_reverse_maps = {
+    "accuracy": accuracy_score.__name__,
+    "recall": recall_score.__name__,
+    "precision": precision_score.__name__,
+    "roc_auc": roc_auc_score.__name__,
+}
+
+
+def rebuild_cv_metric_name(cv_score_metric, eval_mode):
+    prefix = "train-"
+    if eval_mode == "test":
+        prefix = "val-"
+
+    starting_idx = len(eval_mode) + 1
+    cv_metric_name = cv_score_metric[starting_idx:]
+    try:
+        metric_name = f"{prefix}{scoring_reverse_maps[cv_metric_name]}"
+    except KeyError:
+        logging.error('Unknown metric name: "{}"'.format(cv_metric_name))
+    else:
+        return metric_name
+
+
+def convert_cv_scores_to_logging_scores(cv_scores):
+    logging_scores = {}
+    for cv_score_metric, cv_score in cv_scores.items():
+        if str.startswith(cv_score_metric, "train"):
+            eval_mode = "train"
+        elif str.startswith(cv_score_metric, "test"):
+            eval_mode = "test"
+        else:
+            raise ValueError(
+                "Unknown cv_score_metric prefix: {}".format(cv_score_metric)
+            )
+        metric_name = rebuild_cv_metric_name(cv_score_metric, eval_mode)
+        logging_scores[metric_name] = float(cv_score.mean())
+
+    return logging_scores
+
+
 def eval_roc_auc_display(estimator, X, y, fig_path, use_mlflow=False):
     import matplotlib.pyplot as plt
     from sklearn.metrics import RocCurveDisplay
