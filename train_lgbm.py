454df9350f8754e78e1682c70facc6a513a386fb
Eli Chen <ita3051@gmail.com>
2022-06-25 21:56:20 +0800

    Remove redundant code

--- train_lgbm.py
+++ train_lgbm.py
@@ -30,15 +30,7 @@ if __name__ == "__main__":
     }
     model_class = LGBMClassifier
     scoring_funcs = (accuracy_score, recall_score, precision_score, roc_auc_score)
-    scoring = [scoring_maps[metric_func] for metric_func in scoring_funcs]
     evaluator = Evaluator(scoring_funcs=scoring_funcs, use_mlflow=MLFLOW)
-    cv_params = {
-        "n_jobs": 16,
-        "cv": 5,
-        "scoring": scoring,
-        "return_train_score": True,
-        "verbose": True,
-    }
     # NOTICE: We should only evalute the testing set performance once
     # use eval_testing=False for tuning hyperparameters
     # use eval_testing=True for reporting final performance for a specfic model
@@ -51,15 +43,4 @@ if __name__ == "__main__":
         save_trained_model=SAVE_MODEL,
         use_mlflow=MLFLOW,
     )
-    # trainer = LightGBMCVTrainer(
-    #     model_class,
-    #     model_params,
-    #     exp_params=exp_params,
-    #     cv_params=cv_params,
-    #     scoring_funcs=scoring_funcs,
-    #     evaluator=evaluator,
-    #     eval_testing=EVAL_TESTING,
-    #     use_mlflow=MLFLOW,
-    #     save_testing_model=SAVE_MODEL,
-    # )
     trainer.run()
