5653bc840610bf5c6edf96dc3d425d20bde5c3a2
Eli Chen <ita3051@gmail.com>
2022-07-04 02:35:17 +0800

    Update comment

--- demo_inference.py
+++ demo_inference.py
@@ -5,18 +5,20 @@ import pandas as pd
 from preprocessing import VaniilaLGBMPreprocessor
 from inference import ProbabilisticBinaryClassifier
 
+
 data = pd.read_csv("merged_data/brfss_combine_test_v2.csv")
 preprocessor = VaniilaLGBMPreprocessor(data_mode="testing")
 data = preprocessor.preprocess_df(data)
-first_row = data.loc[[0]]
-first_row = first_row.to_numpy()
 
 # Numpy data is acceptable for inference
 # we dont't need to provide the column information for the model
 # In other words, we don't need to wrap the data in a DataFrame
+first_row = data.loc[[0]]
+first_row = first_row.to_numpy()
+
+# Here, we use an Inference Wrapper to adjust the threshold for inference
 model = joblib.load("models/LGBMClassifier-testing-2022-07-04 01:36:33.461375.pkl")
 model = ProbabilisticBinaryClassifier(model, prob_threshold=0.3)
-
 
 # You can use sklearn-compatible API to do infernence
 pred = model.predict(first_row)
