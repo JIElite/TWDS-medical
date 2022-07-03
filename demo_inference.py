f725c8c67b85f57fa8a859eb35a8f5bf41ca5dd2
Eli Chen <ita3051@gmail.com>
2022-07-04 02:31:51 +0800

    Update demo_inference with inference Wrapper

--- demo_inference.py
+++ demo_inference.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 
 from preprocessing import VaniilaLGBMPreprocessor
-
+from inference import ProbabilisticBinaryClassifier
 
 data = pd.read_csv("merged_data/brfss_combine_test_v2.csv")
 preprocessor = VaniilaLGBMPreprocessor(data_mode="testing")
@@ -15,5 +15,13 @@ first_row = first_row.to_numpy()
 # we dont't need to provide the column information for the model
 # In other words, we don't need to wrap the data in a DataFrame
 model = joblib.load("models/LGBMClassifier-testing-2022-07-04 01:36:33.461375.pkl")
+model = ProbabilisticBinaryClassifier(model, prob_threshold=0.3)
+
+
+# You can use sklearn-compatible API to do infernence
 pred = model.predict(first_row)
+print(pred)
+
+# Or we also support Pytorch-compatible API (callable)
+pred = model(first_row)
 print(pred)
