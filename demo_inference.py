45642e30b0fbec9206048ef51f31ac39617ed793
Eli Chen <ita3051@gmail.com>
2022-07-05 07:51:51 +0800

    Fix reading path

--- demo_inference.py
+++ demo_inference.py
@@ -1,5 +1,4 @@
 import os
-os.path.append('..')
 
 import joblib
 
@@ -9,7 +8,7 @@ from flare.preprocessing import VaniilaLGBMPreprocessor
 from flare.inference import ProbabilisticBinaryClassifier
 
 
-data = pd.read_csv("../merged_data/brfss_combine_test_v2.csv")
+data = pd.read_csv("merged_data/brfss_combine_test_v2.csv")
 preprocessor = VaniilaLGBMPreprocessor(data_mode="testing")
 data = preprocessor.preprocess_df(data)
 
@@ -20,7 +19,7 @@ first_row = data.loc[[0]]
 first_row = first_row.to_numpy()
 
 # Here, we use an Inference Wrapper to adjust the threshold for inference
-model = joblib.load("../models/LGBMClassifier-testing-2022-07-04 01:36:33.461375.pkl")
+model = joblib.load("models/LGBMClassifier-testing-2022-07-04 01:36:33.461375.pkl")
 model = ProbabilisticBinaryClassifier(model, prob_threshold=0.3)
 
 # You can use sklearn-compatible API to do infernence
